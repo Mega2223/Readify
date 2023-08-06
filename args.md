88c9029a50a045965ce10cc56dd6265aa1151bdd
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-08-06 02:51:58 -0300

    Proper command line task management

---
+++ args.md
@@ -0,0 +1,4 @@
+## List of application arguments
+
+**-es:** Will read the next strings as endsong file paths until a new command is found  
+**-sh:** Will read the next strings as StreamHistory object paths  
--- src/main/java/net/mega2223/readify/Application.java
+++ src/main/java/net/mega2223/readify/Application.java
@@ -1,6 +1,5 @@
 package net.mega2223.readify;
 
-import net.mega2223.readify.objects.SongHistory;
 import net.mega2223.readify.util.JsonConverter;
 import net.mega2223.readify.util.Misc;
 import net.mega2223.readify.windows.ApplicationWindow;
@@ -22,17 +21,23 @@ public class Application {
      * Add font configuring to the frontend (font chooser and parameter)
      * Perhaps a saveable settings file might help?
      * Data overview board
+     * Do tasks automatically based on commmand line arguments
      * */
     public static void main(String[] args) throws IOException, ParseException {
         ApplicationWindow window = new ApplicationWindow();
-        if (args.length > 0){
-            window.statusLabel.setText(Misc.HTMLize("Loading pre-specified endsong files...\nThis may take a minute"));
-            window.pack();
-        }
-        for (String arg : args) {
-            String data = Misc.readFromFile(new File(arg), null);
-            SongHistory ac = JsonConverter.convertFromEndSongFormat(data);
-            window.songHistory.loadSongs(ac);
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]){
+                case "-es": i++; window.statusLabel.setText("Loading pre-specified endsong files...\nThis may take a minute");
+                    while(args.length > i && args[i].toCharArray()[0] != '-'){
+                        window.songHistory.loadSongs(JsonConverter.convertFromEndSongFormat(Misc.readFromFile(new File(args[i]),null)));
+                        i++;
+                    }
+                case "-sh": i++; window.statusLabel.setText("Loading pre-specified streaming history files...\nThis may take a while");
+                    while(args.length > i && args[i].toCharArray()[0] != '-'){
+                        window.songHistory.loadSongs(JsonConverter.convertFromStreamingHistoryFormat(Misc.readFromFile(new File(args[i]),null)));
+                        i++;
+                    }
+            }
         }
         window.refreshSongStats();
     }
